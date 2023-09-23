9d13bd89682ced4a67ad10c80a09d96d1b92da48
NAM GIBEOM <rlqja0523@naver.com>
2023-09-24 04:44:30 +0900

    feat: s3 설정

---
+++ s3/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "client-log" {
+  bucket = "dangjang.client.log"
+}
+
+resource "aws_s3_bucket" "server-log" {
+  bucket = "dangjang.server.log"
+}
+
+resource "aws_s3_bucket" "user-image" {
+  bucket = "dangjang.user.image"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "dangjang.terraform.tfstate"
+}
