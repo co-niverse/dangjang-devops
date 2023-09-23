8c8d6844b327e8048a1a2973754123ce8b121ad1
NAM GIBEOM <rlqja0523@naver.com>
2023-09-24 06:26:00 +0900

    refactor: 파일명 변경

---
+++ ecr/main.tf
@@ -0,0 +1,30 @@
+resource "aws_ecr_repository" "prod" {
+  name                 = "prod"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository_policy" "prod" {
+  repository = aws_ecr_repository.prod.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+  }
+EOF
+}
