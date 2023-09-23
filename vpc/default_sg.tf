bd168d1b3b5afa6353f534a38e12898815166dec
NAM GIBEOM <rlqja0523@naver.com>
2023-09-24 04:46:39 +0900

    fix: 오타 수정

--- vpc/default_sg.tf
+++ vpc/default_sg.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "prod" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0s.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   lifecycle {
