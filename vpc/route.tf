42f90893c0e771099a442d57c92f4758de090d01
NAM GIBEOM <rlqja0523@naver.com>
2023-09-24 04:46:39 +0900

    fix: 오타 수정

--- vpc/route.tf
+++ vpc/route.tf
@@ -10,5 +10,5 @@ resource "aws_route" "public" {
 resource "aws_route" "private_nat" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.ids
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
