5d3bc86b9ab862ab5f2e38a5996673c49c5054a6
NAM GIBEOM <rlqja0523@naver.com>
2023-09-25 01:41:02 +0900

    feat: s3, ecr 추가

--- project/production/main.tf
+++ project/production/main.tf
@@ -1,23 +1,34 @@
 module "network" {
-  source             = "./vpc"
-  availability_zones = var.availability_zones
-  aws_region         = var.aws_region
+  source = "../../modules/vpc"
+
+  availability_zones      = var.availability_zones
+  aws_region              = var.aws_region
+  cidr_numeral            = var.cidr_numeral
+  cidr_numeral_public     = var.cidr_numeral_public
+  cidr_numeral_private    = var.cidr_numeral_private
+  cidr_numeral_private_db = var.cidr_numeral_private_db
+  env                     = var.env
+  # elb = module
 }
 
 module "s3" {
-  source = "./s3"
+  source = "../../modules/s3"
+
+  env = var.env
 }
 
 module "ecr" {
-  source = "./ecr"
+  source = "../../modules/ecr"
+
+  env = var.env
 }
 
 # module "ecs" {
 #   source = "./ecs"
 # }
 
-module "elb" {
-  source             = "./elb"
-  availability_zones = var.availability_zones
-  aws_region         = var.aws_region
-}
+# module "elb" {
+#   source             = "../../modules/elb"
+#   availability_zones = var.availability_zones
+#   aws_region         = var.aws_region
+# }
---
+++ project/production/terraform.tfvars
@@ -0,0 +1,22 @@
+### Common
+availability_zones = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
+aws_region         = "ap-northeast-2"
+env                = "prod"
+
+### VPC
+cidr_numeral       = "0"
+cidr_numeral_public = {
+  "0" = "0"
+  "1" = "16"
+  "2" = "32"
+}
+cidr_numeral_private = {
+  "0" = "48"
+  "1" = "64"
+  "2" = "80"
+}
+cidr_numeral_private_db = {
+  "0" = "96"
+  "1" = "112"
+  "2" = "128"
+}
