ff5a9ed28750551a4dd90d92b8cb50d0812ac7d6
NAM GIBEOM <rlqja0523@naver.com>
2023-09-25 01:31:41 +0900

    feat: vpc

---
+++ modules/vpc/security_group.tf
@@ -0,0 +1,129 @@
+locals {
+  private_subnet_cidrs = [for subnet in aws_subnet.private : subnet.cidr_block]
+}
+
+# Default Security Group
+resource "aws_security_group" "default" {
+  name   = "default-sg-${var.env}"
+  vpc_id = aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "default-sg-${var.env}"
+  }
+}
+
+# Application Security Group
+resource "aws_security_group" "app" {
+  name   = "app-sg-${var.env}"
+  vpc_id = aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.elb_cidr]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.elb_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "app-sg-${var.env}"
+  }
+}
+
+# RDS Security Group
+resource "aws_security_group" "rds" {
+  name   = "rds-sg-${var.env}"
+  vpc_id = aws_vpc.default.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = local.private_subnet_cidrs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "rds-sg-${var.env}"
+  }
+}
+
+# MongoDB Security Group
+resource "aws_security_group" "mongo" {
+  name   = "mongo-sg-${var.env}"
+  vpc_id = aws_vpc.default.id
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = local.private_subnet_cidrs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "mongo-sg-${var.env}"
+  }
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -6,39 +6,27 @@ variable "aws_region" {
   type = string
 }
 
-variable "vpc_name" {
-  type    = string
-  default = "dangjang"
+variable "env" {
+  type = string
 }
 
 variable "cidr_numeral" {
   type    = string
-  default = "0"
 }
 
 variable "cidr_numeral_public" {
   type = map(string)
-  default = {
-    "0" = "0"
-    "1" = "16"
-    "2" = "32"
-  }
 }
 
 variable "cidr_numeral_private" {
   type = map(string)
-  default = {
-    "0" = "48"
-    "1" = "64"
-    "2" = "80"
-  }
 }
 
 variable "cidr_numeral_private_db" {
   type = map(string)
-  default = {
-    "0" = "96"
-    "1" = "112"
-    "2" = "128"
-  }
+}
+
+variable "elb_cidr" {
+  type = string
+  default = "0.0.0.0/0"
 }
