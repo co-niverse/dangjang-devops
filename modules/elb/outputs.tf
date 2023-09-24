658e35b734c0357be70905bc142e960cacaa9197
NAM GIBEOM <rlqja0523@naver.com>
2023-09-25 04:00:01 +0900

    feat: route53 module

--- modules/elb/outputs.tf
+++ modules/elb/outputs.tf
@@ -1,3 +1,11 @@
 output "elb_target_group_arn" {
   value = aws_alb_target_group.default.arn
 }
+
+output "elb_dns_name" {
+  value = aws_lb.default.dns_name
+}
+
+output "elb_zone_id" {
+  value = aws_lb.default.zone_id
+}
