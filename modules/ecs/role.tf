c28d8b5b08c3178445626661181c0534bd19d6f0
NAM GIBEOM <rlqja0523@naver.com>
2023-09-26 15:51:42 +0900

    refactor: 이름 수정

--- modules/ecs/role.tf
+++ modules/ecs/role.tf
@@ -2,6 +2,7 @@
 #       ECS       #
 ###################
 
+### ECS task
 resource "aws_iam_role" "ecs_task_role" {
   name = "ecs-task-role"
 
@@ -50,8 +51,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
   )
 }
 
-resource "aws_iam_role" "ecs_test_execution_role" {
-  name = "ecs-test-execution-role"
+### ECS task execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -67,7 +69,7 @@ resource "aws_iam_role" "ecs_test_execution_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_test_execution_role_policy" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_test_execution_role.name
+  role       = aws_iam_role.ecs_task_execution_role.name
 }
