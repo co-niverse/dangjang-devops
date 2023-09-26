6afdf5a762d4c335359d3933b31c735d11d2a5b5
NAM GIBEOM <rlqja0523@naver.com>
2023-09-26 16:17:19 +0900

    refactor: output 명 변경

--- modules/kinesis/outputs.tf
+++ modules/kinesis/outputs.tf
@@ -2,12 +2,12 @@
 #     Kinesis     #
 ###################
 
-output "client_log" {
-  value = aws_kinesis_stream.client_log.id
+output "client_log_arn" {
+  value = aws_kinesis_stream.client_log.arn
 }
 
-output "server_log" {
-  value = aws_kinesis_stream.server_log.id
+output "server_log_arn" {
+  value = aws_kinesis_stream.server_log.arn
 }
 
 output "kinesis_role_arn" {
