6320b2c46b013ceb4e544d6d54998914ce98566b
NAM GIBEOM <rlqja0523@naver.com>
2023-09-26 15:54:58 +0900

    feat: s3 bucket output

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,3 +1,15 @@
 ###################
 #       S3        #
 ###################
+
+output "client_log_arn" {
+  value = aws_s3_bucket.client-log.arn
+}
+
+output "server_log_arn" {
+  value = aws_s3_bucket.server-log.arn
+}
+
+output "user_image_arn" {
+  value = aws_s3_bucket.user-image.arn
+}
