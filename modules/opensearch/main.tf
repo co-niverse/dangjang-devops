1fb7ecb4cda694578fb3c1d9750d2bf505467325
NAM GIBEOM <rlqja0523@naver.com>
2023-09-27 20:48:30 +0900

    fix: dashboard 접근 수정

--- modules/opensearch/main.tf
+++ modules/opensearch/main.tf
@@ -2,40 +2,10 @@
 #   OpenSearch    #
 ###################
 
-data "aws_caller_identity" "current" {}
-
-# OpenSearch policy
-data "aws_iam_policy_document" "log_opensearch_policy" {
-  statement {
-    actions = [
-      "es:*"
-    ]
-
-    resources = [
-      "arn:aws:es:${var.aws_region}:${data.aws_caller_identity.current.account_id}:domain/os-log-${var.env}/*"
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values = [
-        "arn:aws:firehose:${var.aws_region}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.firehose_client_log_opensearch_name}${var.env}",
-        "arn:aws:firehose:${var.aws_region}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.firehose_server_log_opensearch_name}${var.env}"
-      ]
-    }
-  }
-}
-
 # OpenSearch domain
 resource "aws_opensearch_domain" "log_opensearch" {
-  domain_name     = "os-log-${var.env}"
-  engine_version  = "OpenSearch_2.7"
-  access_policies = data.aws_iam_policy_document.log_opensearch_policy.json
+  domain_name    = "os-log-${var.env}"
+  engine_version = "OpenSearch_2.7"
 
   cluster_config {
     instance_type  = var.instance_type
@@ -48,12 +18,11 @@ resource "aws_opensearch_domain" "log_opensearch" {
   }
 
   advanced_security_options {
-    anonymous_auth_enabled         = true
-    enabled                        = false
+    enabled                        = true
     internal_user_database_enabled = true
 
     master_user_options {
-      master_user_name     = var.master_user_name
+      master_user_name = var.master_user_name
       master_user_password = var.master_user_password
     }
   }
@@ -64,5 +33,65 @@ resource "aws_opensearch_domain" "log_opensearch" {
 
   domain_endpoint_options {
     enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+  }
+
+  node_to_node_encryption {
+    enabled = true
+  }
+
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_opensearch_domain_policy" "log_opensearch" {
+  domain_name = aws_opensearch_domain.log_opensearch.domain_name
+  access_policies = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = [
+          "es:*"
+        ]
+        Resource = "${aws_opensearch_domain.log_opensearch.arn}/*"
+      }
+    ]
+  })
+}
+
+      # {
+      #   Effect = "Allow"
+      #   Principal = {
+      #     AWS = "*"
+      #   }
+      #   Action = [
+      #     "es:*"
+      #   ]
+      #   Resource = "${aws_opensearch_domain.log_opensearch.arn}/*"
+      #   Condition = {
+      #     ArnEquals = {
+      #       "aws:SourceArn" : [
+      #         "arn:aws:firehose:${var.aws_region}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.firehose_client_log_opensearch_name}${var.env}",
+      #         "arn:aws:firehose:${var.aws_region}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.firehose_server_log_opensearch_name}${var.env}"
+      #       ]
+      #     }
+      #   }
+      # },
+      # {
+      #   Effect = "Allow"
+      #   Principal = {
+      #     AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/*"
+      #   }
+      #   Action = [
+      #     "es:ESHttpGet"
+      #   ]
+      #   Resource = "${aws_opensearch_domain.log_opensearch.arn}/*"
+      # }
