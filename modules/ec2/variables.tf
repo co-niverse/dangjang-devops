2cb6d1735207022d04bd97d6e3b9b2cfc6c77527
NAM GIBEOM <rlqja0523@naver.com>
2023-09-28 01:03:15 +0900

    feat: ec2 module

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,27 @@
+###################
+#       EC2       #
+###################
+
+variable "env" {
+  type = string
+}
+
+variable "mongo_instance_type" {
+  type = string
+}
+
+variable "private_db_subnets" {
+  type = list(string)
+}
+
+variable "mongo_security_group_id" {
+  type = string
+}
+
+variable "public_bastion_subnet" {
+  type = string
+}
+
+variable "default_security_group_id" {
+  type = string
+}
