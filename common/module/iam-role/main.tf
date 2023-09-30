1f05bf350ae611c90f6e2eabb7c052d5b27a358f
NAM GIBEOM <rlqja0523@naver.com>
2023-09-30 14:46:42 +0900

    feat: common iam module

---
+++ common/module/iam-role/main.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_role" "role" {
+  name = "${var.role_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = var.services
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "role" {
+  count = length(var.role_policy_arns)
+  role = aws_iam_role.role.name
+  policy_arn = element(var.role_policy_arns, count.index)
+}
