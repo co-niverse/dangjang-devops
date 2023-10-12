8e972e4726a15fbe4f3d4a3a24844514295bfab8
NAM GIBEOM <rlqja0523@naver.com>
2023-10-13 02:52:04 +0900

    feat: notification kinesis 추가

--- modules/kinesis/main.tf
+++ modules/kinesis/main.tf
@@ -4,7 +4,7 @@
 
 resource "aws_kinesis_stream" "client_log" {
   name             = "kn-client-log-${var.env}"
-  shard_count      = var.shard_count
+  shard_count      = var.log_shard_count
 
   stream_mode_details {
     stream_mode = "PROVISIONED"
@@ -13,7 +13,16 @@ resource "aws_kinesis_stream" "client_log" {
 
 resource "aws_kinesis_stream" "server_log" {
   name             = "kn-server-log-${var.env}"
-  shard_count      = var.shard_count
+  shard_count      = var.log_shard_count
+
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+}
+
+resource "aws_kinesis_stream" "notification" {
+  name             = "kn-notification-${var.env}"
+  shard_count      = var.notification_shard_count
 
   stream_mode_details {
     stream_mode = "PROVISIONED"
--- modules/kinesis/outputs.tf
+++ modules/kinesis/outputs.tf
@@ -9,3 +9,7 @@ output "client_log_arn" {
 output "server_log_arn" {
   value = aws_kinesis_stream.server_log.arn
 }
+
+output "notification_arn" {
+  value = aws_kinesis_stream.notification.arn
+}
