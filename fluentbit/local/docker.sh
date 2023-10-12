591d22e5b65ad53a75e1ff3619220d5c79e88f25
NAM GIBEOM <rlqja0523@naver.com>
2023-10-13 04:36:47 +0900

    fix: fluent bit local cotainer port mapping 제거

--- fluentbit/local/docker.sh
+++ fluentbit/local/docker.sh
@@ -4,4 +4,4 @@ docker build -t fluentbit-local:latest .
 docker stop fluentbit-local
 docker rm fluentbit-local
 
-docker run -d --name fluentbit-local -p 8888:8888 -p 8889:8889 -p 8890:8890 --network test-network fluentbit-local:latest
+docker run -d --name fluentbit-local -p 8888:8888 --network test-network fluentbit-local:latest
