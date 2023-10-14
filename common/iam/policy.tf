f5bccb7118227be9f96a40715fb5fa2baf1915c9
NAM GIBEOM <rlqja0523@naver.com>
2023-10-14 20:06:42 +0900

    refactor: role, policy 분리

---
+++ common/iam/policy.tf
@@ -0,0 +1,43 @@
+module "log_group_policy" {
+  source      = "../module/iam-policy"
+  policy_name = "dangjang-log-group-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+module "kinesis_policy" {
+  source      = "../module/iam-policy"
+  policy_name = "dangjang-kinesis-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "kinesis:DescribeStream",
+          "kinesis:GetShardIterator",
+          "kinesis:GetRecords",
+          "kinesis:DescribeStreamSummary",
+          "kinesis:ListShards",
+          "kinesis:ListStreams",
+          "kinesis:SubscribeToShard"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
---
+++ common/iam/role.tf
@@ -0,0 +1,22 @@
+module "vpc_flow_log_role" {
+  source    = "../module/iam-role"
+  role_name = "vpc-flow-log-role"
+  services = [
+    "vpc-flow-logs.amazonaws.com"
+  ]
+  role_policy_arns = [
+    module.log_group_policy.arn
+  ]
+}
+
+module "notification_lambda_role" {
+  source    = "../../common/module/iam-role"
+  role_name = "notification-lambda-role"
+  services = [
+    "lambda.amazonaws.com"
+  ]
+  role_policy_arns = [
+    module.log_group_policy.arn,
+    module.kinesis_policy.arn
+  ]
+}
