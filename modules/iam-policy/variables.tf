ae17911841f1301226a14594b4c020d3712b85ce
NAM GIBEOM <rlqja0523@naver.com>
2023-10-21 19:40:24 +0900

    docs: description 추가

--- modules/iam-policy/variables.tf
+++ modules/iam-policy/variables.tf
@@ -1,7 +1,9 @@
 variable "policy_name" {
+  description = "policy 이름"
   type = string
 }
   
 variable "policy" {
+  description = "json 형태의 policy"
   type = string
 }
--- modules/iam-role/variables.tf
+++ modules/iam-role/variables.tf
@@ -1,11 +1,14 @@
 variable "role_name" {
+  description = "role 이름"
   type = string
 }
 
 variable "services" {
+  description = "role을 사용할 서비스"
   type = list(string)
 }
   
 variable "role_policy_arns" {
+  description = "role에 연결할 policy arn"
   type = list(string)
 }
