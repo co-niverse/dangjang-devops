1f4cd28c0664d65af8eb6812938634916f434371
NAM GIBEOM <rlqja0523@naver.com>
2023-11-27 23:42:09 +0900

    refactor: subnet, route 모듈 분리

---
+++ modules/vpc/subnet/main.tf
@@ -0,0 +1,49 @@
+# Create subnets
+resource "aws_subnet" "subnet" {
+  for_each                = var.subnets
+  vpc_id                  = var.vpc_id
+  cidr_block              = "10.${var.vpc_cidr_numeral}.${each.value.numeral}.0/24"
+  availability_zone       = each.value.availability_zone
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  tags = {
+    Name = each.key
+  }
+}
+
+# Create route table
+resource "aws_route_table" "route_table" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = var.route_table_name
+  }
+}
+
+# Create route
+resource "aws_route" "internet_gateway" {
+  count                  = var.enable_igw_destination ? 1 : 0
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = var.destination_cidr_block
+  gateway_id             = var.igw_id
+}
+
+resource "aws_route" "nat_gateway" {
+  count                  = var.enable_nat_destination ? 1 : 0
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = var.destination_cidr_block
+  nat_gateway_id         = var.nat_gateway_id
+}
+
+resource "aws_route" "vpc_endpoint" {
+  count                  = var.enable_vpc_endpoint_destination ? 1 : 0
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = var.destination_cidr_block
+  vpc_endpoint_id        = var.vpc_endpoint_id
+}
+
+# Associate route table to subnets
+resource "aws_route_table_association" "route_table_association" {
+  for_each       = aws_subnet.subnet
+  subnet_id      = each.key
+  route_table_id = aws_route_table.route_table.id
+}
---
+++ modules/vpc/subnet/variables.tf
@@ -0,0 +1,70 @@
+variable "vpc_id" {
+  description = "연결할 vpc id"
+  type        = string
+}
+
+variable "vpc_cidr_numeral" {
+  description = "VPC CIDR 넘버"
+  type        = string
+}
+
+variable "subnets" {
+  description = "서브넷 CIDR 넘버, 가용 영역"
+  type = map(object({
+    numeral           = number
+    availability_zone = string
+  }))
+}
+
+variable "map_public_ip_on_launch" {
+  description = "public ip 할당 여부"
+  type        = bool
+  default     = false
+}
+
+variable "route_table_name" {
+  description = "라우트 테이블 이름"
+  type        = string
+}
+
+variable "destination_cidr_block" {
+  description = "라우트 대상 CIDR 블록"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "enable_igw_destination" {
+  description = "인터넷 게이트웨이 목적지 연결 여부"
+  type        = bool
+  default     = false
+}
+
+variable "igw_id" {
+  description = "인터넷 게이트웨이 id"
+  type        = string
+  default = null
+}
+
+variable "enable_nat_destination" {
+  description = "NAT 게이트웨이 목적지 연결 여부"
+  type        = bool
+  default     = false
+}
+
+variable "nat_gateway_id" {
+  description = "NAT 게이트웨이 id"
+  type        = string
+  default = null
+}
+
+variable "enable_vpc_endpoint_destination" {
+  description = "VPC 엔드포인트 목적지 연결 여부"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_endpoint_id" {
+  description = "VPC 엔드포인트 id"
+  type        = string
+  default = null
+}
