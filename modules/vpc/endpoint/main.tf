2c14af519256906315b03c5b1df35e7acf5a5922
NAM GIBEOM <rlqja0523@naver.com>
2023-11-27 18:35:26 +0900

    refactor: vpc endpoint 모듈 분리

---
+++ modules/vpc/endpoint/main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc_endpoint" "gateway" {
+  count = var.create_gateway ? 1 : 0
+  vpc_id  = var.vpc_id
+  service_name = var.service_name
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = var.route_table_ids
+
+  tags = {
+    Name = var.endpoint_name
+  }
+}
+
+resource "aws_vpc_endpoint" "interface" {
+  count = var.create_interface ? 1 : 0
+  vpc_id  = var.vpc_id
+  service_name = var.service_name
+  vpc_endpoint_type = "Interface"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = var.endpoint_name
+  }
+}
