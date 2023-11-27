d77b90663e1d92e8f0cc088bc3ea128cdbd0394b
NAM GIBEOM <rlqja0523@naver.com>
2023-11-28 02:28:52 +0900

    fix: CIDR block 적용

--- modules/vpc/security_group/main.tf
+++ modules/vpc/security_group/main.tf
@@ -12,6 +12,7 @@ resource "aws_security_group" "sg" {
       from_port       = ingress.value.from_port
       to_port         = ingress.value.to_port
       protocol        = ingress.value.protocol
+      cidr_blocks     = ingress.value.cidr_blocks
       security_groups = ingress.value.security_groups
     }
   }
@@ -24,10 +25,6 @@ resource "aws_security_group" "sg" {
       protocol    = egress.value.protocol
       cidr_blocks = egress.value.cidr_blocks
     }
-  }
-
-  lifecycle {
-    create_before_destroy = true
   }
 
   tags = {
