7cbdc812a69288e86e7e6d6ee27e799ef71b9050
NAM GIBEOM <rlqja0523@naver.com>
2023-12-01 23:38:13 +0900

    feat: ec2 instance launch template 모듈 구현

---
+++ modules/ec2/launch-template/main.tf
@@ -0,0 +1,48 @@
+data "template_file" "user_data" {
+  template = file("${path.module}/user_data.sh")
+  vars = {
+    ecs_cluster_name = var.ecs_cluster_name
+  }
+}
+
+data "aws_iam_role" "ec2_role" {
+  name = var.ec2_role_name
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = var.profile_name
+  role = data.aws_iam_role.ec2_role.name
+}
+
+resource "aws_launch_template" "template" {
+  name                   = var.template_name
+  image_id               = var.image_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  user_data              = base64encode(data.template_file.user_data.rendered)
+
+  block_device_mappings {
+    device_name = var.device_name
+
+    ebs {
+      volume_type = var.volume_type
+      volume_size = var.volume_size
+    }
+  }
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.profile.arn
+  }
+
+  monitoring {
+    enabled = var.enabled_monitoring
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+    tags = {
+      Name = var.ebs_tag_name
+    }
+  }
+}
---
+++ modules/ec2/launch-template/variables.tf
@@ -0,0 +1,68 @@
+variable "ecs_cluster_name" {
+  description = "ECS 클러스터 이름"
+  type        = string
+}
+
+variable "ec2_role_name" {
+  description = "EC2 역할 이름"
+  type        = string
+}
+
+variable "profile_name" {
+  description = "인스턴스 프로파일 이름 (IAM 역할을 EC2 인스턴스에 연결)"
+  type        = string
+}
+
+variable "template_name" {
+  description = "시작 템플릿 이름"
+  type        = string
+}
+
+variable "image_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "인스턴스 타입"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key-pair name"
+  type        = string
+}
+
+variable "vpc_security_group_ids" {
+  description = "할당할 Security Group ID"
+  type        = list(string)
+}
+
+variable "device_name" {
+  description = "마운트할 디바이스 이름"
+  type        = string
+  default     = "/dev/sda1"
+}
+
+variable "volume_type" {
+  description = "EBS 볼륨 타입"
+  type        = string
+  default     = "gp3"
+}
+
+variable "volume_size" {
+  description = "EBS 볼륨 크기"
+  type        = number
+  default     = 8
+}
+
+variable "enabled_monitoring" {
+  description = "모니터링 활성화 여부"
+  type        = bool
+  default     = false
+}
+
+variable "ebs_tag_name" {
+  description = "EBS 볼륨 태그 이름"
+  type        = string
+}
